5dcfe9151c1d3f2e505529202e32c0aeecc8a90a
Marc Intha <marc.inthaamnouay@gmail.com>
2018-10-01 22:46:31 +0200

    Initial Commit

---
+++ nantrial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nantrial
+//
+//  Created by Marc Intha on 01/10/2018.
+//  Copyright © 2018 Marc Intha. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
